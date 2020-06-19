34415d0078e95f8ec592bd385f598e2711cec30e
celltea <catherine.galovich@gmail.com>
2020-06-19 17:50:32 -0400

    Update datedifference.py
    added milliseconds to smaller outputs along with some code cleanup

--- utilities/datedifference.py
+++ utilities/datedifference.py
@@ -7,6 +7,7 @@ def date_difference(pastest, presentest=None, offset=None):
         offset = dt.seconds + (dt.days * 60*60*24)
 
     if offset:
+        delta_ms = math.trunc(dt.microseconds / 1000)
         delta_s = math.trunc(offset % 60)
         offset /= 60
         delta_mi = math.trunc(offset % 60)
@@ -14,13 +15,13 @@ def date_difference(pastest, presentest=None, offset=None):
         delta_h = math.trunc(offset % 24)
         offset /= 24
         delta_d = math.trunc(offset % 30)
-        offset /= 30
+        offset /= 30 #I know months aren't standard to 30 days, I also don't need that level of accuracy if it's in the range of months
         delta_mo = math.trunc(offset % 12)
         offset /= 12
         delta_y = math.trunc(offset)
     
     else:
-        raise(ValueError("Must supply otherdate or offset (from now)"))
+        raise(ValueError("Must supply presentest or offset (from now)"))
     
     if delta_y >= 1:
         return(f'{delta_y} year(s), {delta_mo} month(s), {delta_d} day(s)')   
@@ -33,4 +34,4 @@ def date_difference(pastest, presentest=None, offset=None):
     if delta_mi >= 1:
         return(f'{delta_mi} minute(s), {delta_s} second(s)')
     else:
-        return(f'{delta_s} second(s)')
+        return(f'{delta_s} second(s), {delta_ms} millisecond(s)')
